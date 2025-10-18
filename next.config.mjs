0fe24e216a416530e426407a4b385af27ab0410b
mathewjedeikin <mathewjedeikin@gmail.com>
2025-10-17 17:57:24 -0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
-  async redirects() {
-    return [
-      // Redirect /privacy → /#privacy
-      {
-        source: "/privacy",
-        destination: "/#privacy",
-        permanent: true,
-      },
-      // Redirect /terms → /#terms
-      {
-        source: "/terms",
-        destination: "/#terms",
-        permanent: true,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
